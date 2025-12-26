1d46fb169add73d2f78e90d0e72c0a59e67f2afc
Fifin <fifinagustiana05@gmail.com>
2025-12-26 14:52:25 +0700

    fit : menambahkan Comment.php

--- app/models/Comment.php
+++ app/models/Comment.php
@@ -3,16 +3,26 @@
 class Comment
 {
     private $conn;
-    private $table = "comments";
+    private $table = "komentar"; // ✅ sesuai DB
 
     public function __construct($db)
     {
         $this->conn = $db;
     }
 
+    // Ambil semua komentar
     public function getAll()
     {
         $query = "SELECT * FROM {$this->table} ORDER BY created_at DESC";
         return $this->conn->query($query);
+    }
+
+    // Simpan komentar
+    public function insert($isi_komentar)
+    {
+        $query = "INSERT INTO {$this->table} (isi_komentar) VALUES (:isi_komentar)";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':isi_komentar', $isi_komentar);
+        return $stmt->execute();
     }
 }
