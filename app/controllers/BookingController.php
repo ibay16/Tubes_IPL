8c9b6e9ca0701cf4fbf1389306656397be1c7ac7
Fifin <fifinagustiana05@gmail.com>
2025-12-26 16:23:59 +0700

    fit : memperbaiki BookingController.php

--- app/controllers/BookingController.php
+++ app/controllers/BookingController.php
@@ -1,9 +1,29 @@
 <?php
 
-class BookingController
+require_once 'app/models/Booking.php';
+require_once 'app/models/Comment.php';
+
+class AdminController
 {
+    private $db;
+
+    public function __construct()
+    {
+        // database.php HARUS mengembalikan PDO
+        $this->db = Database::connect();
+    }
+
     public function index()
     {
-        require_once 'app/views/booking.php';
+        // Model Booking
+        $bookingModel = new Booking($this->db);
+        $bookings = $bookingModel->getAll();
+
+        // Model Comment
+        $commentModel = new Comment($this->db);
+        $comments = $commentModel->getAll();
+
+        // Kirim ke view
+        require_once 'app/views/admin.php';
     }
 }
